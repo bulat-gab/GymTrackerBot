a398649e6a8475b2cafa92e6d211e17d5e120854
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-05-20 23:09:29 +0600

    add pages

---
+++ src/GymTrackerBot.Domain/Interfaces/IGymServiceStandalone.cs
@@ -0,0 +1,54 @@
+﻿using GymTrackerBot.Domain.Models;
+
+namespace GymTrackerBot.Domain.Interfaces;
+public interface IGymServiceStandalone
+{
+    /// <summary>
+    /// Gets all gym sessions
+    /// </summary>
+    Task<List<GymSession>> GetSessionsAsync();
+
+    /// <summary>
+    /// Gets a specific gym session by ID
+    /// </summary>
+    /// <param name="id">The ID of the session to retrieve</param>
+    Task<GymSession?> GetSessionAsync(int id);
+
+    /// <summary>
+    /// Gets all dates in a specific month that have gym sessions
+    /// </summary>
+    /// <param name="month">The month to check for sessions</param>
+    Task<List<DateTime>> GetSessionDatesInMonthAsync(DateTime month);
+
+    /// <summary>
+    /// Gets all gym sessions for a specific date
+    /// </summary>
+    /// <param name="date">The date to retrieve sessions for</param>
+    Task<List<GymSession>> GetSessionsForDateAsync(DateTime date);
+
+    /// <summary>
+    /// Creates a new gym session
+    /// </summary>
+    /// <param name="session">The session to create</param>
+    Task<GymSession> CreateSessionAsync(GymSession session);
+
+    /// <summary>
+    /// Updates an existing gym session
+    /// </summary>
+    /// <param name="session">The session with updated information</param>
+    Task<GymSession> UpdateSessionAsync(GymSession session);
+
+    /// <summary>
+    /// Deletes a gym session by ID
+    /// </summary>
+    /// <param name="id">The ID of the session to delete</param>
+    Task<bool> DeleteSessionAsync(int id);
+
+    /// <summary>
+    /// Marks a gym session as finished
+    /// </summary>
+    /// <param name="id">The ID of the session to finish</param>
+    /// <param name="endTime">The end time of the session</param>
+    /// <param name="notes">Optional notes to add to the session</param>
+    Task<GymSession> FinishSessionAsync(int id, DateTime endTime, string? notes = null);
+}
---
+++ src/GymTrackerBot.Domain/Versioning/IVersionedModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GymTrackerBot.Domain.Versioning
+{
+    /// <summary>
+    /// Interface for models that support versioning
+    /// </summary>
+    public interface IVersionedModel
+    {
+        /// <summary>
+        /// Gets the schema version of this model
+        /// </summary>
+        int SchemaVersion { get; }
+        
+        /// <summary>
+        /// Upgrades this model instance to the latest schema version
+        /// </summary>
+        /// <returns>An upgraded instance of the model</returns>
+        IVersionedModel UpgradeToLatest();
+    }
+}

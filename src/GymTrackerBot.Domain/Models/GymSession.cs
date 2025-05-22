878aed5c469a9f42851816d4ed0debe000fa523d
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-05-22 08:01:18 +0600

    add session type

--- src/GymTrackerBot.Domain/Models/GymSession.cs
+++ src/GymTrackerBot.Domain/Models/GymSession.cs
@@ -2,18 +2,6 @@
 
 namespace GymTrackerBot.Domain.Models;
 
-// public class GymSession
-// {
-//     public int Id { get; set; }
-//     public long UserId { get; set; }
-//     public string? Username { get; set; }
-//     public DateTime StartTime { get; set; }
-//     public DateTime? EndTime { get; set; }
-//     public string? Notes { get; set; }
-//
-//     public bool IsFinished() => EndTime.HasValue;
-// }
-
 public class GymSession : IVersionedModel
 {
     public static readonly int CurrentVersion = 0;
@@ -23,6 +11,8 @@ public class GymSession : IVersionedModel
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; }
     public string? Notes { get; set; }
+    public GymSessionType SessionType { get; set; } = GymSessionType.None;
+
     // public List<Exercise> Exercises { get; set; } = new();
     
     public bool IsFinished() => EndTime.HasValue;
---
+++ src/GymTrackerBot.Domain/Models/GymSessionType.cs
@@ -0,0 +1,16 @@
+﻿namespace GymTrackerBot.Domain.Models;
+
+[Flags]
+public enum GymSessionType
+{
+    None = 0,
+    Legs = 1 << 0,
+    Chest = 1 << 1,
+    Back = 1 << 2,
+    Shoulders = 1 << 3,
+    Core = 1 << 4,
+    CrossFit = 1 << 5,
+    Cardio = 1 << 6,
+    FullBody = 1 << 7,
+    Mobility = 1 << 8
+}

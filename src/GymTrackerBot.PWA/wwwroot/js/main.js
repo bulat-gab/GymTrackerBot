b2d79897db5bfc5475a46e587ce8e87d2e4b589a
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-05-28 16:40:02 +0600

    add export to file

---
+++ src/GymTrackerBot.PWA/wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿function downloadFile(fileName, contentType, content) {
+    const blob = new Blob([content], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    URL.revokeObjectURL(url); // Clean up after download
+}

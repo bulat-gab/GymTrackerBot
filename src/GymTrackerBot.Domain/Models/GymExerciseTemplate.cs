e4238a6501a5b2f96976092f57c96d10dc99c18b
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-05-29 23:25:10 +0600

    Add Exercises

--- src/GymTrackerBot.Domain/Models/GymExerciseTemplate.cs
+++ src/GymTrackerBot.Domain/Models/GymExerciseTemplate.cs
@@ -1,11 +1,10 @@
 ﻿namespace GymTrackerBot.Domain.Models;
 
-public class ExerciseTemplate
+public class GymExerciseTemplate
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = string.Empty;
     public GymSessionType SessionType { get; set; } // stored as int in JSON
-    public string Equipment { get; set; } = string.Empty;
     public int DefaultSets { get; set; }
     public int DefaultReps { get; set; }
 }

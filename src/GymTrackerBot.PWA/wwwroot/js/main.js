8dca69cabe772fa2806881678f5428e28227666b
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-06-03 17:38:44 +0500

    Add Import json button

--- src/GymTrackerBot.PWA/wwwroot/js/main.js
+++ src/GymTrackerBot.PWA/wwwroot/js/main.js
@@ -7,3 +7,30 @@
     a.click();
     URL.revokeObjectURL(url); // Clean up after download
 }
+
+async function importFile() {
+    return new Promise((resolve) => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+
+        input.addEventListener('change', (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    resolve(e.target.result);
+                };
+                reader.onerror = () => {
+                    console.error("Error reading file.");
+                    resolve(null);
+                };
+                reader.readAsText(file);
+            } else {
+                resolve(null);
+            }
+        });
+
+        input.click();
+    });
+}

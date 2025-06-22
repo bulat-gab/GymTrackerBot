605b7f08a945f6637d7154af7d3a5d1a2cc8ee98
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-06-22 14:25:59 +0300

    add Profile page with build info

---
+++ src/GymTrackerBot.Domain/BuildMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace GymTrackerBot.Domain;
+
+public class BuildMetadata
+{
+    public string CommitHash { get; set; } = "";
+    public string CommitMessage { get; set; } = "";
+    public string Date { get; set; } = "";
+}

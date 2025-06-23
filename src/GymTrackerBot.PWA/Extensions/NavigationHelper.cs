e6107bfea0d1f6e2b6f81130dd9953a911cd6fa5
bulat-gab <14952031+bulat-gab@users.noreply.github.com>
2025-06-23 22:29:06 +0300

    Navigate to a specific session from Calendar page

---
+++ src/GymTrackerBot.PWA/Extensions/NavigationHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GymTrackerBot.PWA.Extensions;
+
+public static class NavigationExtensions
+{
+    public static void NavigateToSession(this NavigationManager navigationManager, int sessionId)
+    {
+        navigationManager.NavigateTo($"sessions/{sessionId}");
+    }
+}
